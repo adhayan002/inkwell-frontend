6d97ce938cc0311bce7ecd59d59598b2f5f41d7d
Adhayan Sharma <96379455+adhayan002@users.noreply.github.com>
2023-10-10 23:56:07 +0530

    Update IndexPage.jsx

--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -4,7 +4,7 @@ import {useEffect, useState} from "react";
 export default function IndexPage() {
   const [posts,setPosts] = useState([]);
   useEffect(() => {
-    fetch('https://backend-blog-jwod.onrender.com/post').then(response => {
+    fetch('https://drab-hen-slippers.cyclic.app//post').then(response => {
       response.json().then(posts => {
         setPosts(posts);
       });
