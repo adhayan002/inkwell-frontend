e4856f42b1a5e4570c8d81f5fc9a7d503789c020
Adhayan Sharma <96379455+adhayan002@users.noreply.github.com>
2023-10-11 00:10:45 +0530

    Update PostPage.jsx

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -14,7 +14,7 @@ const PostPage = () => {
   const navigate = useNavigate();
   useEffect(() => {
     console.log(id);
-    fetch(`https://drab-hen-slippers.cyclic.app/post/${id}`)
+    fetch(`https://backend-blog-jwod.onrender.com/post/${id}`)
       .then((response) => response.json())
       .then((postInfo) => {
         setPostInfo(postInfo);
