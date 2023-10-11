b7d8096b93fe5416f8f0cdf06d1c68f97cf4a536
Adhayan Sharma <96379455+adhayan002@users.noreply.github.com>
2023-10-11 17:05:18 +0530

    Update LoginPage.jsx

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -9,7 +9,7 @@ function LoginPage() {
   const {setUserInfo}=useContext(UserContext)
   async function login(ev) {
     ev.preventDefault();
-    const response = await fetch('https://fullstack-blog-api-eight.vercel.app/login', {
+    const response = await fetch('https://backend-blog-jwod.onrender.com/login', {
       method: 'POST',
       body: JSON.stringify({username, password}),
       headers: {'Content-Type':'application/json'},
