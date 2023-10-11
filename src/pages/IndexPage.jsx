d5359d36bf3106e97a95baec94f4d92f8df25a07
Adhayan Sharma <96379455+adhayan002@users.noreply.github.com>
2023-10-11 17:30:20 +0530

    Update IndexPage.jsx

--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -4,7 +4,7 @@ import {useEffect, useState} from "react";
 export default function IndexPage() {
   const [posts,setPosts] = useState([]);
   useEffect(() => {
-    fetch('https://blog-api-inkwell.adaptable.app//post').then(response => {
+    fetch('https://backend-blog-jwod.onrender.com/post').then(response => {
       response.json().then(posts => {
         setPosts(posts);
       });
